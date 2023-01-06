5ec139977982cd633a200974f6a265d6990e1efc
bbax23 <b.bax23@hotmail.co.uk>
2023-01-06 10:15:45 +0000

    update how to use skymap text

--- src/components/HowToUse.jsx
+++ src/components/HowToUse.jsx
@@ -7,7 +7,7 @@ const HowToUse = () => {
     const popover = (<Popover id="popover-basic">
         <Popover.Header as="h3">How to use </Popover.Header>
         <Popover.Body>
-            In the search bar below, you can search for deep sky objects from the Messier Catalogue by their Messier number e.g M1, or by their name if they have one. <br />
+            In the search bar to the right, you can search for deep sky objects from the Messier Catalogue by their Messier number e.g M1, or by their name if they have one. <br />
             Simply start typing and choose from the list that appears to perform the search and view the target on the sky map. <br />
             The clear button will clear the search bar ready for you to type again. <br />
             Your current target is displayed above the top left of the map.
